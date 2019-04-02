a790ccb5d720e105100776529eac144e87aa16bd
gfike19@gmail.com <Vaughn*573>
2019-04-01 19:14:52 -0500

    working on unit 5

---
+++ Unit5/CP1_PHP.php
@@ -0,0 +1,19 @@
+<?php
+
+$Score_John;
+$Score_Paul;
+
+$Score_John = 90;
+$Score_Paul = 75;
+
+echo "Score_John = ".$Score_John;
+echo "<br>Score_Paul = ".$Score_Paul."<br>";
+
+if($Score_John > $Score_Paul) {
+    echo "John wins";
+}
+else {
+    echo "Paul wins";
+}
+
+?>
