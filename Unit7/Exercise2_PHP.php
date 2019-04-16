d9e266290bdb2dc555856dab095c17fc63d4ab71
gfike19@gmail.com <Vaughn*573>
2019-04-16 13:36:51 -0500

    finished exercises

--- Unit7/Exercise2_PHP.php
+++ Unit7/Exercise2_PHP.php
@@ -11,7 +11,6 @@ $num1 = $_POST['num1'];
 $num2 = $_POST['num2'];
 $num3 = $_POST['num3'];
 echo "If input is ".$num1.", ".$num2.", ".$num3."<br>";
-echo "Sum is: ".SumThree($num1, $num2, $num3)."<br>";
 echo "Average: ".Avg($num2, $num2, $num3);
 
 ?>
